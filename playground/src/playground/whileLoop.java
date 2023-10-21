bdd0276cb9f4f4ef3e1f6d61bff6e33f4b78a5bd
Azka Raki <azkaraqy552@gmail.com>
2023-10-21 17:40:06 +0800

    belajar do-while & while loop

--- playground/src/playground/whileLoop.java
+++ playground/src/playground/whileLoop.java
@@ -1,15 +1,76 @@
 package playground;
+
 import java.util.Scanner;
 
 public class whileLoop {
 
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter perulangan : ");
-        int n = input.nextInt(), i = 1;
-        while (i <= n) {            
-            System.out.println("Perulangan ke-" + i);
+//        int i = 1, j = 1, n;
+//        System.out.print("Enter ada berapa perkalian : ");
+//        int n = input.nextInt();
+//        while (i <= n) {            
+//            System.out.println("Perulangan ke-" + i);
+//            i++;
+//        }
+//        do {
+//            if (n < 1) {
+//                System.out.println("error");
+//            } else {
+//                System.out.println(i);
+//                i++;
+//            }
+//        } while (i <= n);
+//        System.out.println("");
+//        while (i <= n) {
+//            System.out.println("Perkalian " + i);
+//            while (j <= 10) {                
+//                System.out.println(i + " x " + j + " = " + (i*j));
+//                j++;
+//            }
+//            System.out.println("");
+//            i++;
+//            j = 1;
+//        }
+//
+        System.out.print("Enter baris : ");
+        int n = input.nextInt();
+        int i = 1;
+//        do {
+//            if (n < 10) {
+//                int k = n;
+//                do {
+//                    System.out.print(" ");
+//                    k--;
+//                } while (k >= i);
+//                int j = 1;
+//                do {
+//                    System.out.print(i + " ");
+//                    j++;
+//                } while (j <= i);
+//                System.out.println("");
+//                i++;
+//            } else {
+//                System.out.println("Jika input baris lebih dari 9 maka formasinya akan tidak rapi");
+//                break;
+//            }
+//
+//        } while (i <= n);
+        while (i <= n) {
+            int k = n;
+            while (k >= i) {
+                System.out.print(" ");
+                k--;
+            }
+            int j = 1;
+            while (j <= i) {
+                System.out.print(i + " ");
+//                System.out.print(j + " ");
+                j++;
+            }
+            System.out.println("");
             i++;
         }
+
     }
 }
