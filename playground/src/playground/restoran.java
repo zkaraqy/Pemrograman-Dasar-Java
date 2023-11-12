0c09e221597d9245bf6b3602bb80d9646f4592a1
Azka Raki <azkaraqy552@gmail.com>
2023-11-12 21:35:27 +0800

    Menambahkan batasan diskon yang didapat yaitu maks 25% dan merapikan output

--- playground/src/playground/restoran.java
+++ playground/src/playground/restoran.java
@@ -46,22 +46,28 @@ public class restoran {
 
     static double totalDiskon(double diskon, int tagihanMakanan, int tagihanMinuman) {
         if (tagihanMakanan + tagihanMinuman >= 100000) {
-            return diskon * Math.floor((tagihanMakanan + tagihanMinuman) / (100000));
+            diskon = diskon * (int) ((tagihanMakanan + tagihanMinuman) / (100000));
+            if (diskon * 100 <= 25.0) {
+                return diskon;
+            } else {
+                diskon = 0.25;
+                return diskon;
+            }
         } else {
             return 0;
         }
     }
 
     static void menu() {
-        System.out.println("\tMenu Makanan: ");
-        System.out.println("1. Nasi Bumbu Ayam  Rp.16,000");
-        System.out.println("2. Nasi Bumbu Bebek Rp.18,000");
-        System.out.println("3. Spagetti         Rp.18,000");
-        System.out.println("4. Soup Ayam        Rp.15,000");
-        System.out.println("\n\tMenu Minuman: ");
-        System.out.println("1. Teh Es           Rp.3,000");
-        System.out.println("2, Susu             Rp.5,000");
-        System.out.println("3. Jus Mangga       Rp.5,000");
+        System.out.println("\t       Menu Makanan: ");
+        System.out.println("1. Nasi Bumbu Ayam  \t\tRp.16,000");
+        System.out.println("2. Nasi Bumbu Bebek \t\tRp.18,000");
+        System.out.println("3. Spagetti         \t\tRp.18,000");
+        System.out.println("4. Soup Ayam        \t\tRp.15,000");
+        System.out.println("\n\t       Menu Minuman: ");
+        System.out.println("1. Teh Es           \t\tRp.3,000");
+        System.out.println("2. Susu             \t\tRp.5,000");
+        System.out.println("3. Jus Mangga       \t\tRp.5,000");
     }
 
     public static void main(String args[]) {
@@ -79,36 +85,39 @@ public class restoran {
             System.out.print("Password\t: ");
             inputPasssword = input.nextLine();
             if (inputUsername.equals("Muhammad Azka Raki") && inputPasssword.equals("2311016110005")) {
+                System.out.println("");
                 System.out.println("=========================================");
                 System.out.println("      Selamat Datang di Restoran Zka     ");
                 System.out.println("=========================================");
                 menu();
                 System.out.println("=========================================");
-                System.out.print("Nomor pilihan makanan: ");
+                System.out.print("Nomor pilihan makanan : ");
                 pilihanMakanan = input.nextInt();
-                System.out.print("Jumlah makanan       : ");
+                System.out.print("Jumlah makanan        : ");
                 jumlahMakanan = input.nextInt();
-                System.out.print("Nomor pilihan minuman: ");
+                System.out.print("Nomor pilihan minuman : ");
                 pilihanMinuman = input.nextInt();
-                System.out.print("Jumlah minuman       : ");
+                System.out.print("Jumlah minuman        : ");
                 jumlahMinuman = input.nextInt();
                 System.out.println("=========================================");
+
+                System.out.println("\t\tTAGIHAN");
 
                 tagihanMakanan = tagihanMakanan(pilihanMakanan, jumlahMakanan);
                 tagihanMinuman = tagihanMinuman(pilihanMinuman, jumlahMinuman);
 
                 switch (pilihanMakanan) {
                     case 1:
-                        System.out.println("Nasi Bumbu Ayam  x" + jumlahMakanan + "\t| Rp." + tagihanMakanan);
+                        System.out.println("Nasi Bumbu Ayam  |x" + jumlahMakanan + "\t| Rp." + tagihanMakanan);
                         break;
                     case 2:
-                        System.out.println("Nasi Bumbu Bebek x" + jumlahMakanan + "\t| Rp." + tagihanMakanan);
+                        System.out.println("Nasi Bumbu Bebek |x" + jumlahMakanan + "\t| Rp." + tagihanMakanan);
                         break;
                     case 3:
-                        System.out.println("Spagetti         x" + jumlahMakanan + "\t| Rp." + tagihanMakanan);
+                        System.out.println("Spagetti         |x" + jumlahMakanan + "\t| Rp." + tagihanMakanan);
                         break;
                     case 4:
-                        System.out.println("Soup Ayam        x" + jumlahMakanan + " \t| Rp." + tagihanMakanan);
+                        System.out.println("Soup Ayam        |x" + jumlahMakanan + "\t| Rp." + tagihanMakanan);
                         break;
                     default:
                         System.out.println("Pilihan menu makanan Invalid");
@@ -116,22 +125,23 @@ public class restoran {
 
                 switch (pilihanMinuman) {
                     case 1:
-                        System.out.println("Teh Es           x" + jumlahMinuman + "\t| Rp." + tagihanMinuman);
+                        System.out.println("Teh Es           |x" + jumlahMinuman + "\t| Rp." + tagihanMinuman);
                         break;
                     case 2:
-                        System.out.println("Susu             x" + jumlahMinuman + "\t| Rp." + tagihanMinuman);
+                        System.out.println("Susu             |x" + jumlahMinuman + "\t| Rp." + tagihanMinuman);
                         break;
                     case 3:
-                        System.out.println("Jus Mangga       x" + jumlahMinuman + "\t| Rp." + tagihanMinuman);
+                        System.out.println("Jus Mangga       |x" + jumlahMinuman + "\t| Rp." + tagihanMinuman);
                         break;
                     default:
                         System.out.println("Pilihan menu minuman Invalid");
                 }
 
-                System.out.println("Diskon            \t| " + totalDiskon(diskon, tagihanMakanan, tagihanMinuman));
-                System.out.println("Total Tagihan     \t= Rp." + ((tagihanMakanan + tagihanMinuman) - (tagihanMakanan + tagihanMinuman) * totalDiskon(diskon, tagihanMakanan, tagihanMinuman)));
+                System.out.println("Diskon                  | " + (totalDiskon(diskon, tagihanMakanan, tagihanMinuman) * 100) + "%");
+                System.out.println("Total Tagihan           | Rp." + ((tagihanMakanan + tagihanMinuman) - (tagihanMakanan + tagihanMinuman) * totalDiskon(diskon, tagihanMakanan, tagihanMinuman)));
                 System.out.println("=========================================");
-                System.out.println("   Terima Kasih Telah Berkunjung   ");
+                System.out.println("      Terima Kasih Telah Berkunjung      ");
+                System.out.println("");
                 break;
             } else {
                 percobaan++;
